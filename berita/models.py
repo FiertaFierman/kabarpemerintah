18de14dda15b50b4db750306d5fa187543e84da7
firmanarif58 <firmanarif58@gmail.com>
2025-08-19 13:32:38 +0700

    update

--- berita/models.py
+++ berita/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from ckeditor.fields import RichTextField
 
 class Kategori(models.Model):
     nama = models.CharField(max_length=100)
@@ -17,7 +18,7 @@ class Kategori(models.Model):
 class Berita(models.Model):
     judul = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    isi = models.TextField()
+    isi = RichTextField()
     gambar = models.ImageField(upload_to='gambar/')
     penulis = models.ForeignKey(User, on_delete=models.CASCADE)
     tanggal_publish = models.DateTimeField(auto_now_add=True)
--- berita_pemerintah/settings.py
+++ berita_pemerintah/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'berita',
+    "ckeditor",
 ]
 
 MIDDLEWARE = [
