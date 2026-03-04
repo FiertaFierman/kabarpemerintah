14b79bffd56d3c21ea6bcce939ea3eb8b8f61f5e
firmanarif58 <firmanarif58@gmail.com>
2026-03-04 12:50:49 +0700

    update

--- berita_pemerintah/settings.py
+++ berita_pemerintah/settings.py
@@ -25,13 +25,7 @@ SECRET_KEY = 's#9^kcczlkp4i0csj!l5+0^p5idbftdikbw$ab(g%z=f@$nfzy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'e0f1f298c5a8.ngrok-free.app',
-    '38.147.122.65',
-    'kabarpemerintah.com',
-]
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://e0f1f298c5a8.ngrok-free.app',  # <- tambahkan ini
